729ddf2c8ed300a061669cb66059efc4a4fef920
Dayllan de Souza Alho <dayllan.alho@sou.inteli.edu.br>
2022-11-17 15:05:57 -0300

    :white_check_mark: Changing name file

--- Source/Código Fonte - IoT/V1_Wifi - esp.ino
+++ Source/Código Fonte - IoT/V1_Wifi - esp.ino
@@ -2,12 +2,12 @@
 // Change the SSID and PASSWORD here if needed
 const char * WIFI_FTM_SSID = "Rede_quarteto"; //determinando login da rede
 const char * WIFI_FTM_PASS = "Rede_quarteto4"; //senha da rede
-void setup() {
+void setup(){
   Serial.begin(115200);
   Serial.println("Starting SoftAP with FTM Responder support");
   // Enable AP with FTM support (last argument is 'true')
   WiFi.softAP(WIFI_FTM_SSID, WIFI_FTM_PASS, 1, 0, 4, true);
 }
-void loop() {
+void loop(){
   delay(1000);
 }
