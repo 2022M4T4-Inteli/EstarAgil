03e661731a2f721a5fc2f95716bbef5ea2d05947
André Luís Lessa Junior <99192411+andlljr@users.noreply.github.com>
2022-11-30 10:03:26 -0300

    :wrench: Fix frontend dashboard

--- Source/FrontEnd/pages/index.tsx
+++ Source/FrontEnd/pages/index.tsx
@@ -1,17 +1,66 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import ModalComponent from "./components/modal";
 import CardEmpComponent from "./components/CardEmp";
 import CardRowComponent from "./components/CardRow";
 import StackedBarChart from "./components/StackedGraph";
-import { Container, Content } from "./style";
+import { Container, Content, ModalContainer , DetailsModalContainer } from "./style";
 
 function GraphCMSImageLoader({ src, width }: any) {
   const relativeSrc = (src: any) => src.split("/").pop();
 
   return `https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Henry_Cavill_by_Gage_Skidmore.jpg/${src}`;
 }
+
 export default function Dashboard() {
+  const [detailsModalIsOpen, setDetailsModalIsOpen] = useState(false);
+  function openModalDetails() {
+    setDetailsModalIsOpen(true);
+  }
+  function closeModalDetails() {
+    setDetailsModalIsOpen(false);
+  }
   return (
+    <>
+      <ModalComponent
+        modalIsOpen={detailsModalIsOpen}
+        onClose={closeModalDetails}
+      >
+        <DetailsModalContainer>
+          <h2>Tarcisio Souza</h2>
+          <div className="content">
+            <div className="image">
+              <Image
+                src="800px-Henry_Cavill_by_Gage_Skidmore.jpg"
+                width={50}
+                height={50}
+                alt=""
+                loader={GraphCMSImageLoader}
+              />
+            </div>
+            <div className="info">
+              <h2>Estou a caminho!</h2>
+              <br />
+              <p>Chegando em 10 minutos</p>
+
+              <div className="progress">
+                <span className="active"></span>
+                <span className="active"></span>
+                <span></span>
+                <span></span>
+              </div>
+
+              <h4>últimas informações:</h4>
+
+              <p>Prisma: 230</p>
+              <p>Horário de saída: 14:50</p>
+              <p>Ultima corrida: 24/10/2022 às 14:50</p>
+            </div>
+          </div>
+        </DetailsModalContainer>
+      </ModalComponent>
+
     <div>
       <Container>
         <div>
@@ -63,11 +112,14 @@ export default function Dashboard() {
           <br />
           <div className="list-cards">
             {[1, 2, 3, 4, 5, 6].map((e) => (
-              <CardRowComponent key={e} />
+              <CardRowComponent key={e} 
+              onClick={openModalDetails}
+              />
             ))}
           </div>
         </Content>
       </Container>
     </div>
+  </>
   );
 }
