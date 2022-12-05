61b28898957405e6287f7a73380b6a14d116e46b
André Luís Lessa Junior <99192411+andlljr@users.noreply.github.com>
2022-12-05 09:38:34 -0300

    :wrench: Update server settings

--- Source/BackEnd/server.js
+++ Source/BackEnd/server.js
@@ -1,18 +1,22 @@
 // Import libraries
+const { application } = require('express');
 const express = require('express');
 
 // Importing app function
 const app = require('./app');
 
+const localhost = "127.0.0.1";
 
 //Application port
 const port = process.env.PORT || 4005;
 
+const host = process.env.HOST || localhost;
+
 console.clear();
 
 //Printing server status
-app.listen(port,error => {
+app.listen(port, host, error => {
     error?
     console.log(`There was an error while starting server...\n ${error}`) :
-    console.log(`🚀 Server started successfully. Listening http://localhost:${port}.`)
+    console.log(`🚀 Server started successfully. Listening http://${host}:${port}.`)
 })
